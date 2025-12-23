6361e58c75b1425f0d07973baa04d5193f8c8e9d
jangse0 <syjang1016@korea.ac.kr>
2025-12-23 10:52:00 +0900

    Revert "lv2-8: QueryDSL"
    This reverts commit 2db2fbf6dcac1d0893d8fce9e5eeb025a7f1dc44.

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("""
         SELECT t FROM Todo t
@@ -25,9 +25,8 @@ public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRep
                                               @Param("startDate") LocalDate startDate,
                                               @Param("endDate") LocalDate endDate);
 
-    //2-8 QueryDSL로 변경
-    /*@Query("SELECT t FROM Todo t " +
+    @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);*/
+    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
